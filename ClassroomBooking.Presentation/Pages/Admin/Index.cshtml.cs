637a5617c6661c7d60b9a0274edac182ec8625ba
Nguyễn Minh Thành <141386951+ScatterZero@users.noreply.github.com>
2025-03-15 22:43:16 +0700

    Create Admin

---
+++ ClassroomBooking.Presentation/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using ClassroomBooking.Repository.Entities;
+using ClassroomBooking.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClassroomBooking.Presentation.Pages.Admin
+{
+    public class AdminBookingModel : PageModel
+    {
+        private readonly IBookingService _bookingService;
+
+        public AdminBookingModel(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        public List<Booking> BookingList { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (!User.IsInRole("Admin")) // Kiểm tra quyền admin
+            {
+                return RedirectToPage("/Account/Login");
+            }
+
+            // Lấy tất cả booking (Admin xem toàn bộ)
+            BookingList = await _bookingService.GetAllBookingsAsync();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostUpdateStatusAsync(int bookingId, string bookingstatus)
+        {
+            if (!User.IsInRole("Admin"))
+            {
+                return Unauthorized();
+            }
+
+            var success = await _bookingService.UpdateBookingStatusAsync(bookingId, bookingstatus);
+            if (!success)
+            {
+                ModelState.AddModelError("", "Update Failed!");
+            }
+
+            return RedirectToPage();
+        }
+        
+    }
+}
--- ClassroomBooking.Presentation/obj/Debug/net8.0/ClassroomBooking.Presentation.AssemblyInfo.cs
+++ ClassroomBooking.Presentation/obj/Debug/net8.0/ClassroomBooking.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassroomBooking.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b232a08a8ebb4c9e5576d7262ff989223c148218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9455084b16fb073127c8457b2fa1d1639e31a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassroomBooking.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassroomBooking.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
